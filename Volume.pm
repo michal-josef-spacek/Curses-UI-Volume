876d0f737ff85c3ab1a9ccb4962a6ac2dac12681
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2022-12-10 19:16:51 +0100

    Update author name

--- Volume.pm
+++ Volume.pm
@@ -327,13 +327,13 @@ L<https://github.com/tupinek/Curses-UI-Volume>
 
 =head1 AUTHOR
 
-Michal Špaček L<mailto:skim@cpan.org>
+Michal Josef Špaček L<mailto:skim@cpan.org>
 
 L<http://skim.cz>
 
 =head1 LICENSE AND COPYRIGHT
 
-© 2014-2022 Michal Špaček
+© 2014-2022 Michal Josef Špaček
 
 BSD 2-Clause License
 
